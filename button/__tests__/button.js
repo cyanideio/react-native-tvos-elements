a6933c190726c4c5763d1bf5ae5a759a6270e865
Relaed <knarfytrebil@gmail.com>
2018-07-19 13:16:26 +0900

    (test) button, icon

--- button/__tests__/button.js
+++ button/__tests__/button.js
@@ -4,8 +4,28 @@ import toJson from 'enzyme-to-json';
 import TvOSButton from '../../button';
 
 describe('TvOSButton Component', () => {
-    it('should render without issues', () => {
+    it('should render: <title only>', () => {
         const component = shallow(<TvOSButton title='bitcoin' />);
         expect(component.length).toBe(1);
+        expect(toJson(component)).toMatchSnapshot();
+    });
+    it('should render: <icon left>', () => {
+        const component = shallow(<TvOSButton 
+            txtStyleFocused={{color:'green'}}
+            title='Steam' 
+            titleAlign='left' 
+            iconType='Ionicons' 
+            iconName='ios-person' />);
+        expect(component.length).toBe(1);
+        expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('should render: <custom text style>', () => {
+        const component = shallow(<TvOSButton
+            txtStyleFocused={{color:'red'}}
+            txtStyleUnfocused={{color:'black'}}
+            title='Settings' />);
+        expect(component.length).toBe(1);
+        expect(toJson(component)).toMatchSnapshot();
     });
 });
