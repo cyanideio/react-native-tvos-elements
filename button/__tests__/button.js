493d905a4b5b84ff74bfee754d40be3e7d0fdf1e
Relaed <knarfytrebil@gmail.com>
2018-07-19 12:57:10 +0900

    (fix) add first test for button

--- button/__tests__/button.js
+++ button/__tests__/button.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import TvOSButton from '../../button';
+
+describe('TvOSButton Component', () => {
+    it('should render without issues', () => {
+        const component = shallow(<TvOSButton title='bitcoin' />);
+        expect(component.length).toBe(1);
+    });
+});
